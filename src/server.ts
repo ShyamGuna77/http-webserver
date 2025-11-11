afffa77cdd85dcc6676416823b2ba753af99549c
ShyamGuna77 <gshyamprasad2001@gmail.com>
2025-11-11 15:47:25 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,32 @@
 import * as net from "net";
 
-let server = net.createServer();
+function newConn(socket: net.Socket): void {
+  console.log("new connection", socket.remoteAddress, socket.remotePort);
 
-server.listen({ host: "127.0.0.1", port: 3000 }, () => {
-  console.log("Server is running on port 8080");
+  socket.on("end", () => {
+    console.log("EOF.");
+  });
+
+  socket.on("data", (data: Buffer) => {
+    console.log("data:", data.toString());
+    socket.write(data); 
+
+    if (data.includes("q")) {
+      console.log("closing.");
+      socket.end();
+    }
+  });
+}
+
+const server = net.createServer();
+
+server.on("error", (err: Error) => {
+  console.error("Server error:", err);
+  throw err;
+});
+
+server.on("connection", newConn);
+
+server.listen({ host: "127.0.0.1", port: 1234 }, () => {
+  console.log("Server listening on 127.0.0.1:1234");
 });
