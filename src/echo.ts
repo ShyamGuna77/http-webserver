921bc33318dc9f03f194e3fd2b91da067b71b0f8
ShyamGuna77 <gshyamprasad2001@gmail.com>
2025-11-11 17:55:24 +0530

    Implement TCP connection handling in echo server
    - Introduced functions for initializing, reading, and writing TCP connections.
    - Enhanced the server-client interaction to handle data transmission and connection termination.
    - Updated server listening configuration for improved logging.

--- src/echo.ts
+++ src/echo.ts
@@ -1,4 +1,96 @@
-import * as net from "net"
+import { Socket } from "dgram";
+import * as net from "net";
+import { devNull } from "os";
+
+type TCPprops = {
+  socket: net.Socket;
+  err: Error | null;
+  ended: boolean;
+  reader: null | {
+    resolve: (value: Buffer) => void;
+    reject: (reason: Error) => void;
+  };
+};
+
+function IntializeConnection(socket: net.Socket): TCPprops {
+  const connection: TCPprops = {
+    socket,
+    err: null,
+    ended: false,
+    reader: null,
+  };
+  socket.on("data", (data: Buffer) => {
+    if (!connection.reader) return;
+    connection.socket.pause();
+    connection.reader?.resolve(data);
+    connection.reader = null;
+  });
+  socket.on("end", () => {
+    connection.ended = true;
+    if (connection.reader) {
+      connection.reader.resolve(Buffer.from(""));
+      connection.reader = null;
+    }
+  });
+  socket.on("error", (err: Error) => {
+    connection.err = err;
+    if (connection.reader) {
+      connection.reader.reject(err);
+      connection.reader = null;
+    }
+  });
+
+  return connection;
+}
+
+function ReadConnection(connection: TCPprops): Promise<Buffer> {
+  console.assert(!connection.reader, "Concurrent reads are not allowed");
+  return new Promise((resolve, reject) => {
+    if (connection.ended) {
+      resolve(Buffer.from(""));
+      return;
+    }
+
+    if (connection.err) {
+      reject(connection.err);
+      return;
+      }
+      
+      connection.reader = { resolve, reject }
+      connection.socket.resume()
+      
+  });
+}
+
+
+function WriteConnection(connection: TCPprops, data: Buffer): Promise<void> {
+  console.assert(data.length > 0, "Cannot write empty buffer");
+  return new Promise((resolve, reject) => {
+    if (connection.err) {
+      reject(connection.err);
+      return;
+    }
+    connection.socket.write(data, (err: Error | null | undefined) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
+
+
+async function ServerClient(socket: net.Socket): Promise<void> {
+    const connection = IntializeConnection(socket);
+    while (true) {
+        const data = await ReadConnection(connection);
+        if (data.length === 0) {
+            console.log("end connection");
+            socket.destroy();
+            break;
+        }
+         console.log("data:", data.toString());
+        await WriteConnection(connection, data);
+    }
+}
 
 
 
@@ -12,6 +104,6 @@ server.on("error", (err: Error) => {
   throw err;
 });
 
-server.on("connection", (socket: net.Socket) => {
-  console.log("promised baded echo connection", socket.remoteAddress, socket.remotePort);
+server.listen({ host: "127.0.0.1", port: 1234 }, () => {
+  console.log("promised baded echo server ");
 });
